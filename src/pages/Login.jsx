f8ec6a8699ca177fc475a7920fae2e0e796c687c
JULIAN ROJAS <juliandra140201@gmail.com>
2024-01-11 22:55:49 -0500

    First commit

---
+++ src/pages/Login.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+
+function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log(email);
+        console.log(password);
+    }
+  return (
+    <>
+    <form onSubmit={handleSubmit}>
+        <label htmlFor="">Correo</label>
+        <input type="email" name="email" placeholder="youremail@site.com" onChange={(e) => {setEmail(e.target.value)}} />
+        <label htmlFor="">Contraseña</label>
+        <input type="password" name="password" placeholder="yourpassword" onChange={(e) => {setPassword(e.target.value)}} />
+        <button type="submit">Enviar</button>
+    </form>
+    </>
+  )
+}
+
+export default Login
